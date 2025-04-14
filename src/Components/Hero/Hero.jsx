3bcf548a41ab0d9afd62d371078ca7b2297c6e95
Jessica García <viri_grc08@hotmail.com>
2025-04-14 12:54:20 -0600

    Primer hero responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,16 +6,16 @@ const Hero = () => {
     <div className="hero-container">
       <video src="/videos/video-Inicio.mp4" className="hero-video" autoPlay muted loop />
       <div className="hero-text">
-        <img src="/image/Mundo2.png" className="hero-mundo" />
+        <img src="/image/Mundo2.png" className="hero-mundo" alt="Mundo" />
         <h1 className="text1">REDES SEGURAS Y PROTEGIDAS</h1>
         <h2 className="text2">CON</h2>
         <h2 className="text3">2PROTECTION</h2>
-          <p className="text4">
-            Nuestro objetivo es ser facilitadores del proceso de desarrollo tecnológico de su empresa, aportando continuidad operacional, ahorro de costos, estabilidad organizacional, productividad y visibilidad de la seguridad TI.
-          </p>
+        <p className="text4">
+          Nuestro objetivo es ser facilitadores del proceso de desarrollo tecnológico de su empresa, aportando continuidad operacional, ahorro de costos, estabilidad organizacional, productividad y visibilidad de la seguridad TI.
+        </p>
       </div>
       <button className="fortinet-button">
-        <img src="/image/Fortinet-btn.png" alt="" className="button-fortinet" />
+        <img src="/image/Fortinet-btn.png" alt="Fortinet" className="button-fortinet" />
       </button>
     </div>
   );
